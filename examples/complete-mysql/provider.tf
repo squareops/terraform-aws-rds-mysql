9a211641cab8d5e65b1f955c158d26b37ff2432d
Aman <146931382+amanravi-squareops@users.noreply.github.com>
2025-03-12 18:15:44 +0530

    Update provider.tf

--- examples/complete-mysql/provider.tf
+++ examples/complete-mysql/provider.tf
@@ -6,11 +6,11 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = ""
+  name = local.cluster_name
 
 }
 data "aws_eks_cluster_auth" "cluster" {
-  name = ""
+  name = local.cluster_name
 }
 
 provider "kubernetes" {
