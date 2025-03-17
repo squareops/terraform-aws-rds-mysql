4b5cca41f8fdea32ec9c0cc1a53ab1ad567bb024
amanravi-squareops <aman.ravi@squareops.comm>
2025-03-17 11:18:49 +0530

    removed bucket provider

--- examples/complete-mysql/main.tf
+++ examples/complete-mysql/main.tf
@@ -140,11 +140,10 @@ module "rds-mysql" {
   namespace                        = local.namespace
   create_namespace                 = local.create_namespace
   mysqldb_backup_enabled           = false
-  bucket_provider_type             = "s3"
   mysqldb_backup_config = {
     mysql_database_name  = ""                                       # specify the database name to backup or leave it empty to backup all databases
     cron_for_full_backup = "0 */6 * * *"                            # cron expression for full backup
-    bucket_uri           = "s3://atmosly-rds-backup-test/"          # s3 bucket uri
+    bucket_uri           = "s3://atmosly-rds-backup-test/"          # s3 bucket uri with / at the end
   }
   mysqldb_restore_enabled = false
   mysqldb_restore_config = {
